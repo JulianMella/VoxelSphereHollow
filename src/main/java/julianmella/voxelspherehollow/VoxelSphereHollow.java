28be8d7f9748d4d48003bae9316a9780262b75fd
JulianMella <juliaame@uio.no>
2025-09-18 04:44:05 +0200

    feat: block initialization done, texture applied, stored in functional tab, has generic name and properties

--- src/main/java/julianmella/voxelspherehollow/VoxelSphereHollow.java
+++ src/main/java/julianmella/voxelspherehollow/VoxelSphereHollow.java
@@ -1,5 +1,6 @@
 package julianmella.voxelspherehollow;
 
+import julianmella.voxelspherehollow.blocks.VoxelViewerBlock;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -15,10 +16,6 @@ public class VoxelSphereHollow implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		VoxelViewerBlock.initialize();
 	}
 }
