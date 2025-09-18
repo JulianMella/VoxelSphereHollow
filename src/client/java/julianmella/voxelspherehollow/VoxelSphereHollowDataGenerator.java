4d402bfcd98adbb4ec5fa64062ff7536ef1dbd3b
JulianMella <juliaame@uio.no>
2025-09-18 03:15:05 +0200

    initial commit

---
+++ src/client/java/julianmella/voxelspherehollow/VoxelSphereHollowDataGenerator.java
@@ -0,0 +1,11 @@
+package julianmella.voxelspherehollow;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VoxelSphereHollowDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
